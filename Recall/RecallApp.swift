12f782b3c09fd03da2e33e20db27c3fa16edb9de
DakshKaushik <kaushikdaksh03@gmail.com>
2025-04-09 14:52:56 +0530

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Daksh Kaushik on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
